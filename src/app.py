24942c0f918eb4ee827b3006ce6330357eb90412
alicjakr <krupczynskaalicja@gmail.com>
2026-04-10 18:02:36 +0200

    refactor: project strcuture

--- src/app.py
+++ src/app.py
@@ -1,15 +1,9 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 
 app = Dash()
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
 df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
     "Amount": [4, 1, 2, 2, 4, 5],
